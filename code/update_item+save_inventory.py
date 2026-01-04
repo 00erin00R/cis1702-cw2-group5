01ddbce08e3bbb0febdae45de6b7941578453d5e
TPA2007 <tolulopeakinrinlola41@Gmail.com>
2026-01-04 17:29:18 +0000

    update_item and save_inventory functions
    slightly cleaned up the save function

--- code/update_item+save_inventory.py
+++ code/update_item+save_inventory.py
@@ -1,7 +1,7 @@
 #update_item function for updating items selected by the user
 def update_item(inventory):
     try:
-        item_id = int(input("Enter the ID of the item to update: "))
+        item_id = int(input("Enter the ID of the item to update - "))
         for item in inventory:
             if item["id"] == item_id:
                 print("Item found:")
@@ -12,7 +12,7 @@ def update_item(inventory):
                 print("2 - Item price")
                 print("3 - Item stock")
 
-                choice = int(input("Enter your choice (1-3)- "))
+                choice = int(input("Enter your choice (1-3) - "))
 
                 if choice == 1:
                     new_name = input("Please enter the updated name - ")
@@ -36,11 +36,12 @@ def update_item(inventory):
         print("Invalid input. Please enter numbers only.")
 
 
-#save_inventory function to save updates made to the inventory on a updated file
-def save_inventory(filename, data):
+#save_inventory function to save updates made to the inventory on an updated file (e.g updated_inventory.json)
+def save_inventory(filename, inventory):
     try:
         with open(filename, "w") as f:
-            json.dump(data, f, indent=4)
+            json.dump(inventory, f, indent=4)
         print(f"Updated inventory saved to '{filename}'.")
     except Exception as e:
         print(f"Error saving inventory to '{filename}': {e}")
+
