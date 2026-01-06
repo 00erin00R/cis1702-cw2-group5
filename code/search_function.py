b5dcd280355d43d298900eb845d4e3e2f91c620a
Rhys7002 <rhyswjones007@gmail.com>
2026-01-06 11:08:03 +0000

    Create search_function.py
    Just need the function to be called on the main program

---
+++ code/search_function.py
@@ -0,0 +1,18 @@
+def search_item(inventory):
+    name_search = input("Enter item name to search: ") # finds item in file
+
+    found = False
+
+    for item in inventory:
+        if item["name"].lower() == name_search.lower(): # turns input into lowercase characters to remove errors
+
+            # prints item details to user but only if its found in inventory
+            print("Item found:")
+            print("(ID)\tName\t\tPrice\tQuantity")
+            print("--------------------------------------------")
+            print(item["id"], "\t", item["name"], "\t\t", item["price"], "\t", item["quantity"])
+            found = True
+            break
+
+    if found == False: # if theres no item a message is displayed to the user
+        print("Item not found.")
